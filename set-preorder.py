b0da6854bcd20ae98fb7d5d6b72dba279d3123ec
dekosdizayn-sketch <dekosdizayn@gmail.com>
2026-08-22 00:32:13 +0500

    Update set-preorder.py

--- set-preorder.py
+++ set-preorder.py
@@ -4,8 +4,8 @@ token = os.getenv("KASPI_API_TOKEN")
 
 print("DEKOS Kaspi Automation іске қосылды")
 
-env:
-  KASPI_API_TOKEN: ${{ secrets.KASPI_API_TOKEN }}
+if not token:
+    raise Exception("KASPI_API_TOKEN табылмады")
 
 print("Kaspi API токен табылды")
 print("Барлық тауарды 1 күндік предзаказға қою жүйесі дайын")
